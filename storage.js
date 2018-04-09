7d7e62f30b16dc362af0600c5494371849fda0a0
Katheryn Ford <AnEvilHerbivoreLP@gmail.com>
2018-04-09 14:33:20 -0500

    Code cleaned up

--- storage.js
+++ storage.js
@@ -96,11 +96,7 @@ const saveDatabase = function (database, localStorageKey) {
 }
 
 const loadDatabase = function (localStorageKey) {
-    // Get the string version of the database
-    const databaseString = localStorage.getItem(localStorageKey)
-
-    // Use JSON.parse() to convert the string back into an object
-    return JSON.parse(databaseString)
+   return JSON.parse(localStorage.getItem(localStorageKey))
 }
 
 saveDatabase(HomeInventoryDatabase, "HomeInventory")
